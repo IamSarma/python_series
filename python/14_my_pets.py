48c99685ffed9c94954d847b127bb7a38006f303
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-15 22:19:38 +0530

    List examples 👍

---
+++ python/14_my_pets.py
@@ -0,0 +1,9 @@
+my_pets = ["snoopy", "rosie", "goldy"]
+
+print("Enter a pet name: ")
+name = input()
+
+if name not in my_pets:
+    print("I do not have a pet named " + name)
+else:
+    print(name + " is my pet.")
