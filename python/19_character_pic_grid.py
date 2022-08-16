36d7e17b7555912584e221fce219ab4176dd3726
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-16 21:12:23 +0530

    Character picture grid, fun exercise 😎

---
+++ python/19_character_pic_grid.py
@@ -0,0 +1,17 @@
+# Character Picture Grid - Fun exercise 😜
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+# Main program loop
+for y in range(len(grid[0])):
+    for x in range(len(grid)):
+        print(grid[x][y], end="")
+    print()
