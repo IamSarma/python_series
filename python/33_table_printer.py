7f18c951d13b39b5735807c642d2207289b4f514
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-19 23:55:41 +0530

    Base file with sample data is created

---
+++ python/33_table_printer.py
@@ -0,0 +1,12 @@
+# Function to display input in a well-organized tabluar format
+def printTable(table_data):
+    prin()
+
+
+table_data = [
+    ["apples", "oranges", "cherries", "banana"],
+    ["Alice", "Bob", "Carol", "David"],
+    ["dogs", "cats", "mosse", "goose"]
+]
+
+printTable(table_data)
