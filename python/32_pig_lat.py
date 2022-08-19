b8fad65d5be55b9bb15b9b7fbeae0cd0b8a3f4a7
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-19 21:55:35 +0530

    Program to transalte Enligh message to Pig Latin 🐷

---
+++ python/32_pig_lat.py
@@ -0,0 +1,19 @@
+# Pig Latin program 🐷
+# Translates given input sentence to Pig Latin 🐽
+VOWELS = ("a", "e", "i", "o", "u", "y")
+pig_latin = []
+
+print("Enter the English message to translate into Pig Latin:")
+message = input()
+
+# English to Pig Latin
+for word in message.split():
+    # Separate the non-letters at the start of the current word
+    prefix_non_letters = ""
+    while len(word) > 0 and not word[0].isalpha():
+        prefix_non_letters += word[0]
+        word = word[1:]
+
+    if len(word) == 0:
+        pig_latin.append(prefix_non_letters)
+        continue
