44591707950fc383624393ead5f19e71b29ebfa0
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-19 21:39:53 +0530

    Fixed bug in appending start to the text

--- python/31_bullet_point_adder.py
+++ python/31_bullet_point_adder.py
@@ -8,6 +8,7 @@ text = pyperclip.paste()
 # Separate lines and add stars.
 lines = text.split("\n")
 for i in range(len(lines)):
-    lines[i] += "* "
+    lines[i] = "* " + lines[i]
 
+text = "\n".join(lines)
 pyperclip.copy(text)
