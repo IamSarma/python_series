3b339b0ac603a9b7e60ed920c9fc97371052daf1
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-19 23:56:44 +0530

    Finding length of the largest word to use for right alignment

--- python/33_table_printer.py
+++ python/33_table_printer.py
@@ -1,6 +1,11 @@
 # Function to display input in a well-organized tabluar format
 def printTable(table_data):
-    prin()
+    max_len = 0
+    # Finding maximum lenth to perform right justify
+    for data in table_data:
+        for item in data:
+            if len(item) > max_len:
+                max_len = len(item)
 
 
 table_data = [
