ecfe2d79cd552c34091e0f523e4ec32fc3ab89f5
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-20 10:29:54 +0530

    Added logic to validate the second three characters with hypen of input text

--- python/34_is_phone_number.py
+++ python/34_is_phone_number.py
@@ -7,3 +7,8 @@ def isPhoneNumber(text):
     for i in range(0, 3):
         if not text[i].isdecimal():
             return False
+    if text[3] != "-":
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
