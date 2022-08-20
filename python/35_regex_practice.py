87a5dfe014086fee9a8c7e95867fafebbe15110f
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-20 11:07:26 +0530

    Validating and extracting country code and phone number containing paranthesis

--- python/35_regex_practice.py
+++ python/35_regex_practice.py
@@ -3,6 +3,14 @@
 import re
 
 # Basic phone number search
-phonenum_regex = re.compile(r"\d\d\d-\d\d\d-\d\d\d\d")
-match_object = phonenum_regex.search("My number is 123-456-7788")
-print(f"Found number found: {match_object.group()}")
+phonenum_regex_1 = re.compile(r"\d\d\d-\d\d\d-\d\d\d\d")
+match_object_1 = phonenum_regex_1.search("My number is 123-456-7788")
+print(f"Phone number found: {match_object_1.group()}")
+
+
+# Validation of phone number containing paranthesis
+# also helpful to extract country code and number separately
+phonenum_regex_2 = re.compile(r"(\(\d\d\d\)) (\d\d\d-\d\d\d\d)")
+match_object_2 = phonenum_regex_2.search("My number is (123) 456-7890")
+print(f"Country code found : {match_object_2.group(1)}")
+print(f"Phone number found : {match_object_2.group(2)}")
