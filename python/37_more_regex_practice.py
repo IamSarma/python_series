45f49e459caa1350443524ea30e42cdc3603f956
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-21 12:10:31 +0530

    Regex that matches a number with commas for every 3 digits

--- python/37_more_regex_practice.py
+++ python/37_more_regex_practice.py
@@ -5,3 +5,10 @@ import re
 item_string = "12 drummers, 11 pipers, five rings, 3 hens"
 num_regex = re.compile(r"\d+")
 print(num_regex.sub("X", item_string))
+
+
+# Regex that matches a number with commas for every 3 digits
+num_string = "1234"
+comma_regex = re.compile(r"^\d{1,2}(,\d{3})*$")
+match_object_1 = comma_regex.search(num_string)
+print(match_object_1 != None)
