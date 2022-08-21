6d9ff22272e629c98e732fd07cb6dfec838bdfed
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-21 13:30:12 +0530

    Implemented correct logic to identify valid month from 1 to 12 with optional leading 0

--- python/38_date_detection.py
+++ python/38_date_detection.py
@@ -2,7 +2,7 @@
 import re
 
 
-date_regex = re.compile(r"^(3[01]|[12][0-9]|[1-9])$")
+date_regex = re.compile(r"^(3[01]|[12][0-9]|[1-9])/(1[0-2]|0?[1-9])$")
 input_date = input()
 match_object = date_regex.search(input_date)
 if match_object != None:
