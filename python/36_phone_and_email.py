6fb3e17fbe600b90ce481f94b63dd958c1ff8c76
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-21 10:21:53 +0530

    Implemented phone number match regex

---
+++ python/36_phone_and_email.py
@@ -0,0 +1,21 @@
+#! python3
+# phone_and_email.py
+# Finds phone numbers and email addresses on the clipboard
+import re
+
+# Phone number regex
+phone_regex = re.compile(r"""(
+    (\d{3}|\(\d{3}\))?                  # area code
+    (\s|-|\.)?                          # separator
+    (\d{3})                             # first 3 digits
+    (\s|-|\.)                           # separator
+    (\d{4})                             # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))?      # extension
+)""", re.VERBOSE)
+
+
+# Email regex
+
+# Find matches in clipboard tex
+
+# Copy results to the clipboard
