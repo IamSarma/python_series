19687e219b736d8b883e01e17a6f329f6d31315d
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-22 00:43:14 +0530

    Example of using limit argument to limit the response to certain number of times

--- python/42_py_input_plus_args.py
+++ python/42_py_input_plus_args.py
@@ -26,5 +26,10 @@ import pyinputplus as pyip
 
 
 # Argument to use in case of blank input is allowed
-user_response_4 = pyip.inputNum("Enter a number: ", blank=True)
-print(user_response_4)
+# user_response_4 = pyip.inputNum("Enter a number: ", blank=True)
+# print(user_response_4)
+
+
+# Using limit argument
+user_response_5 = pyip.inputNum("Enter a number: ", limit=2)
+print(user_response_5)
