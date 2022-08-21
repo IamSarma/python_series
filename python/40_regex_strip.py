adeb6449838fe7c4644f43933ec768c3bc53cd46
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-21 23:11:11 +0530

    strip function replication using regex

---
+++ python/40_regex_strip.py
@@ -0,0 +1,12 @@
+# Simulating strip() functionality using regex
+# whitespaces to be removed from the beginning and end of the string
+# if second argument is passed then those characters to be removed from the string
+import re
+
+
+def regex_strip(test_string, opt_arg=""):
+    return True
+
+
+test_string = "   python   "
+print(regex_strip(test_string))
