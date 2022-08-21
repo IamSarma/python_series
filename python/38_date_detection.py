082dc4d830de3a9866258b0eb84dadaf97279943
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-21 13:31:58 +0530

    Implemented correct logic to identify valid year form 0000 to 9999

--- python/38_date_detection.py
+++ python/38_date_detection.py
@@ -2,10 +2,10 @@
 import re
 
 
-date_regex = re.compile(r"^(3[01]|[12][0-9]|[1-9])/(1[0-2]|0?[1-9])$")
+date_regex = re.compile(r"^(3[01]|[12][0-9]|[1-9])/(1[0-2]|0?[1-9])/[0-9]{4}$")
 input_date = input()
 match_object = date_regex.search(input_date)
 if match_object != None:
-    print(match_object.group())
+    print("Valid date")
 else:
     print("Invalid date")
