7d0f4bf6b370ecc45bfb256b75ccea3f855cb7c5
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-21 12:19:54 +0530

    Regex that matches full name of someone who has a specific last name

--- python/37_more_regex_practice.py
+++ python/37_more_regex_practice.py
@@ -12,3 +12,17 @@ num_string = "1234"
 comma_regex = re.compile(r"^\d{1,2}(,\d{3})*$")
 match_object_1 = comma_regex.search(num_string)
 print(match_object_1 != None)
+
+
+# Regex that matches full name of someone whose last name is Watanabe
+# First name's first letter should be capitalized
+# Preceding word shouldn't contain a nonletter character
+# First name should exist
+# First character of last name should be capitalized
+full_name = "Haruto Watanabe"
+first_name_regex = re.compile(r"^[A-Z]\w+ Watanabe")
+match_object_2 = first_name_regex.search(full_name)
+if match_object_2 != None:
+    print(match_object_2.group())
+else:
+    print(match_object_2 != None)
