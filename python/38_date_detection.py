330a840a94b6b61dda730e621e0389c5c4a50574
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-21 13:17:11 +0530

    Implemented correct logic to identify valid date from 1 to 31

--- python/38_date_detection.py
+++ python/38_date_detection.py
@@ -2,10 +2,10 @@
 import re
 
 
-date_regex = re.compile(r"\b(1|31)\b/\b(1|12)\b")
+date_regex = re.compile(r"^(3[01]|[12][0-9]|[1-9])$")
 input_date = input()
 match_object = date_regex.search(input_date)
 if match_object != None:
     print(match_object.group())
 else:
-    print("Invalid number")
+    print("Invalid date")
