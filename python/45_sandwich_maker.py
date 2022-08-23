24de278ead357d60b795535269c34a9681ebdb26
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-23 23:42:45 +0530

    Asking user preference for protein

--- python/45_sandwich_maker.py
+++ python/45_sandwich_maker.py
@@ -5,3 +5,8 @@ import pyinputplus as pyip
 # Ask user for bread type
 user_bread_pref = pyip.inputMenu(
     ["wheat", "white", "sourdough"], "Bread type:\n")
+
+
+# Ask user for protein type
+user_protein_pref = pyip.inputMenu(
+    ["chicken", "turkey", "ham", "tofu"], "Protein type:\n")
