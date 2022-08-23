3aa7d205699dc92a140d424f1d1854166c4549de
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-24 00:31:06 +0530

    Using Path to identify folder/file path irrespective of OS

--- python/46_pathlib_example.py
+++ python/46_pathlib_example.py
@@ -3,3 +3,8 @@ from pathlib import Path
 
 print(Path("projects", "python"))
 print(str(Path("projects", "python")))
+
+
+my_files = ["projects.txt", "data.csv", "todo.docx"]
+for file_name in my_files:
+    print(Path(r"C:\Users\MB", file_name))
