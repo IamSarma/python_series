d34ef2b90df33cf996bd5a62c68abadb3a3d9ddc
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-24 00:34:46 +0530

    Using the / operator to join paths

--- python/46_pathlib_example.py
+++ python/46_pathlib_example.py
@@ -1,10 +1,19 @@
 # Usage of pathlib module
 from pathlib import Path
 
+
+# Sample path object(s)
 print(Path("projects", "python"))
 print(str(Path("projects", "python")))
 
 
+# Loop through and generate file path
 my_files = ["projects.txt", "data.csv", "todo.docx"]
 for file_name in my_files:
     print(Path(r"C:\Users\MB", file_name))
+
+
+# Using the / operator to join paths
+print(Path("projects") / "python" / "pathlib")
+print(Path("projects") / Path("python/pathlib"))
+print(Path("projects") / Path("python", "pathlib"))
