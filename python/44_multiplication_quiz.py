867ab523ffeafbd92c85f35bf2a0dae87d4e5131
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-23 20:23:52 +0530

    Implemented logic to pick random value(s) and show the user prompt in loop for number of questions times

--- python/44_multiplication_quiz.py
+++ python/44_multiplication_quiz.py
@@ -3,6 +3,12 @@ import pyinputplus
 import random
 import time
 
-
-num_of_questions = 0
+num_of_questions = 10
 correct_answers = 0
+
+# Pick two random number and show prompt to the user
+for question_num in range(num_of_questions):
+    num_1 = random.randint(0, 9)
+    num_2 = random.randint(0, 9)
+    user_prompt = f"#{question_num + 1}: {num_1} x {num_2} = "
+    print(user_prompt)
