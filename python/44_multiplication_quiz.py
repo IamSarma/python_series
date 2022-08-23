181715b84dd972176b16ce20cc012b33fc5e9705
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-23 20:31:15 +0530

    Implemented allowRegexes, blockRegexes, limit and timeout

--- python/44_multiplication_quiz.py
+++ python/44_multiplication_quiz.py
@@ -1,5 +1,5 @@
 # Multiplication quiz program using pyinputplus
-import pyinputplus
+import pyinputplus as pyip
 import random
 import time
 
@@ -11,4 +11,13 @@ for question_num in range(num_of_questions):
     num_1 = random.randint(0, 9)
     num_2 = random.randint(0, 9)
     user_prompt = f"#{question_num + 1}: {num_1} x {num_2} = "
-    print(user_prompt)
+
+    try:
+        # Right answers are handled by allowRegexes
+        # Wrong answers are handled by blockRegexes with a custom message
+        pyip.inputStr(user_prompt, allowRegexes=[
+                      f"^{num_1 * num_2}$"], blockRegexes=[(".*", "Incorrect!")], timeout=8, limit=3)
+    except pyip.TimeoutException:
+        print("Out of time!")
+    except pyip.RetryLimitException:
+        print("Out of tries")
