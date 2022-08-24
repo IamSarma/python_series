77e0fb0846e5abfb8c851ddb96923018145d45b7
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-24 20:32:32 +0530

    Hanlding absolute and relative paths

--- python/46_pathlib_example.py
+++ python/46_pathlib_example.py
@@ -4,45 +4,52 @@ import os
 
 
 # Sample path object(s)
-print(Path("projects", "python"))
-print(str(Path("projects", "python")))
+# print(Path("projects", "python"))
+# print(str(Path("projects", "python")))
 
 
 # Loop through and generate file path
-my_files = ["projects.txt", "data.csv", "todo.docx"]
-for file_name in my_files:
-    print(Path(r"C:\Users\MB", file_name))
+# my_files = ["projects.txt", "data.csv", "todo.docx"]
+# for file_name in my_files:
+#     print(Path(r"C:\Users\MB", file_name))
 
 
 # Using the / operator to join paths
-print(Path("projects") / "python" / "pathlib")
-print(Path("projects") / Path("python/pathlib"))
-print(Path("projects") / Path("python", "pathlib"))
+# print(Path("projects") / "python" / "pathlib")
+# print(Path("projects") / Path("python/pathlib"))
+# print(Path("projects") / Path("python", "pathlib"))
 
 
 # Example of joining parent and sub folder(s) paths
-parent_folder = Path("C:/Users/MB")
-sub_folder = Path("python_projects")
-print(parent_folder / sub_folder)
-print(str(parent_folder / sub_folder))
+# parent_folder = Path("C:/Users/MB")
+# sub_folder = Path("python_projects")
+# print(parent_folder / sub_folder)
+# print(str(parent_folder / sub_folder))
 
 
 # Current working directory
-print(Path.cwd())
+# print(Path.cwd())
 
 
 # Changing directory path using chdir()
-os.chdir("C:/Windows/System32")
-print(Path.cwd())
+# os.chdir("C:/Windows/System32")
+# print(Path.cwd())
 
 
 # The home directory
-print(Path.home())
+# print(Path.home())
 
 
 # Creating new folders using os.makedirs() function
-os.makedirs("D:/test/sample/python")
+# os.makedirs("D:/test/sample/python")
 
 
 # Creating singe folder using Path.mkdir() function
-Path(r"D:/test_1").mkdir()
+# Path(r"D:/test_1").mkdir()
+
+
+# Handling absolute and relative paths
+print(Path.cwd())
+print(Path.cwd().is_absolute())
+print(Path.cwd() / Path("my/relative/path"))
+print(Path.home() / Path("my/relative/path"))
