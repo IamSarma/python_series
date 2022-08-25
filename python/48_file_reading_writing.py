90369c309b42cac71c85347c40f6381bf27db6d3
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-25 21:24:38 +0530

    Opening file(s) with the open() function

--- python/48_file_reading_writing.py
+++ python/48_file_reading_writing.py
@@ -1,6 +1,10 @@
 # Reading and writing files
 from pathlib import Path
 
-txt_file = Path("test.txt")
-txt_file.write_text("Hello MB!")
-print(txt_file.read_text())
+# txt_file = Path("test.txt")
+# txt_file.write_text("Hello MB!")
+# print(txt_file.read_text())
+
+
+# Opening files with the open() function
+test_file = open(Path.cwd() / "test.txt")
