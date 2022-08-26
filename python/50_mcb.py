731aa314ce603ea89fb5b1f03067497c182079c2
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-27 00:05:27 +0530

    Updated code

--- python/50_mcb.py
+++ python/50_mcb.py
@@ -1,9 +1,9 @@
 #! python3
-# mcb.pyw - Saved and loads pieces of text to the clipboard
+# mcb.py - Saved and loads pieces of text to the clipboard
 # Usage :
-# py.exe mcb.pyw save <keyword> - Saves clipboard to keyword
-# py.exe mcb.pyw <keyword> - Loads keyword to clipboard
-# py.exe mcb.pyw list - Loads all keywords to clipboard
+# py.exe <file_name> save <keyword> - Saves clipboard to keyword
+# py.exe <file_name> <keyword> - Loads keyword to clipboard
+# py.exe <file_name> list - Loads all keywords to clipboard
 import shelve
 import pyperclip
 import sys
