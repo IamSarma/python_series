735f27697f2ec4299b35109b1ed2d37289ee7b58
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-27 23:51:30 +0530

    Takling user input

--- python/52_mad_libs.py
+++ python/52_mad_libs.py
@@ -5,4 +5,10 @@ text_file = open(Path.cwd() / "mad_libs.txt")
 input_string = text_file.read()
 text_file.close()
 
+# Take user input and append the string
+adjective_input = input("Enter an adjective:\n")
+noun_input = input("Enter a noun:\n")
+verb_input = input("Enter a verb:\n")
+noun_input = input("Enter a noun:\n")
+
 print(input_string)
