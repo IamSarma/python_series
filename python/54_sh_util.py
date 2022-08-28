801f131fa60da96d3636703f2e60a8c425e97838
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-28 13:43:29 +0530

    Using shutil module - copying and renaming the file

--- python/54_sh_util.py
+++ python/54_sh_util.py
@@ -6,3 +6,5 @@ from pathlib import Path
 # Copying files and folders
 path_object = Path.home()
 shutil.copy(path_object / "copy_me.txt", path_object / "copy_here")
+shutil.copy(path_object / "copy_me.txt",
+            path_object / "copy_here\iam_copied.txt")
