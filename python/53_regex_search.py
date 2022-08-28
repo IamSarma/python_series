daa9484fb542c036a185752dcaf2855e3475bbc9
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-28 12:59:13 +0530

    Loop through all the text file(s) in the target folder

--- python/53_regex_search.py
+++ python/53_regex_search.py
@@ -9,3 +9,10 @@ target_folder = "C:\\Users\\MB\\Documents\\python_series\\text_files"
 # Ask user to input regex
 user_regex = input("Enter regex to search and display the result:\n")
 user_regex = re.compile(user_regex)
+
+
+# Loop through all the text file(s) in the target folder
+for file_name in os.listdir(target_folder):
+    if file_name.endswith(".txt"):
+        target_file = open(target_folder + "\\" + file_name)
+        target_file.close()
