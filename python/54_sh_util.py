a0eff25c0c40b8ccab9a66480326f195052b2dcb
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-28 13:46:53 +0530

    Copying entire directory/folder

--- python/54_sh_util.py
+++ python/54_sh_util.py
@@ -3,8 +3,12 @@ import shutil
 import os
 from pathlib import Path
 
-# Copying files and folders
+# Copying files
 path_object = Path.home()
-shutil.copy(path_object / "copy_me.txt", path_object / "copy_here")
-shutil.copy(path_object / "copy_me.txt",
-            path_object / "copy_here\iam_copied.txt")
+# shutil.copy(path_object / "copy_me.txt", path_object / "copy_here")
+# shutil.copy(path_object / "copy_me.txt",
+#             path_object / "copy_here\iam_copied.txt")
+
+
+# Copying entire directory/folder
+shutil.copytree(path_object / "copy_me", path_object / "copy_me_backup")
