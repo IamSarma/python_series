360849ba48cb5875d88eaf58c2bd04725d78ea55
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-28 12:58:07 +0530

    Assigning target folder and asking user to input required regex

--- python/53_regex_search.py
+++ python/53_regex_search.py
@@ -3,3 +3,9 @@
 # and print the result to the screen
 import re
 import os
+
+target_folder = "C:\\Users\\MB\\Documents\\python_series\\text_files"
+
+# Ask user to input regex
+user_regex = input("Enter regex to search and display the result:\n")
+user_regex = re.compile(user_regex)
