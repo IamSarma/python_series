10ac69eb4de47800913cfe059ad51562667648ac
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-30 15:20:48 +0530

    Add all the file(s) in the target folder to the ZIP file

--- python/57_folder_backup_project.py
+++ python/57_folder_backup_project.py
@@ -27,4 +27,12 @@ def backupToZip(folder):
         print(f"Adding files in {folder_name}...")
         backup_zip.write(folder_name)
 
+        # Add all the files in this folder to the ZIP file
+        for file_name in file_names:
+            new_base = os.path.basename(folder) + "_"
+            if file_name.startswith(new_base) and file_name.endswith(".zip"):
+                continue        # don't backup the already backed up file(s)
+            backup_zip.write(os.path.join(folder_name, file_name))
+    backup_zip.close()
+
     print("Folder backed up")
