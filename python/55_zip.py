23fde7718e8c5c43f0f0284fdd50908606af39e4
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-30 11:09:45 +0530

    Getting zip info using getinfo() method

--- python/55_zip.py
+++ python/55_zip.py
@@ -5,8 +5,10 @@ from pathlib import Path
 
 # Reading zip file(s)
 home_path = Path.home()
-sample_zip = zipfile.ZipFile(home_path / "copy_me.zip")
-print(sample_zip.namelist())
+sample_zip = zipfile.ZipFile(home_path / "test.zip")
+# print(sample_zip.namelist())
+sample_zip_size = sample_zip.getinfo("test.txt")
+print(sample_zip_size)
 
 # Extracting from zip file(s)
 
