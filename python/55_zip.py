38cef48018c56138bdcd3883123a6b995d78e28c
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-30 11:04:11 +0530

    Listing out folder(s), sub-folder(s) and files in a directory using namelist() method

--- python/55_zip.py
+++ python/55_zip.py
@@ -4,6 +4,9 @@ from pathlib import Path
 
 
 # Reading zip file(s)
+home_path = Path.home()
+sample_zip = zipfile.ZipFile(home_path / "copy_me.zip")
+print(sample_zip.namelist())
 
 # Extracting from zip file(s)
 
