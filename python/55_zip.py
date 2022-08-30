431ad2e18835c6e6cfb8762dbaecaf48ca9fcab0
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-30 11:15:16 +0530

    Reading zip file(s) - code updated

--- python/55_zip.py
+++ python/55_zip.py
@@ -8,8 +8,10 @@ home_path = Path.home()
 sample_zip = zipfile.ZipFile(home_path / "test.zip")
 # print(sample_zip.namelist())
 sample_zip_info = sample_zip.getinfo("test.txt")
-print(sample_zip_info.file_size)
-print(sample_zip_info.compress_size)
+print(f"File size is: {sample_zip_info.file_size} bytes")
+print(f"File compressed size is: {sample_zip_info.compress_size} bytes")
+print(
+    f"Compressed file is {round(sample_zip_info.file_size / sample_zip_info.compress_size, 2)}x smaller")
 
 # Extracting from zip file(s)
 
