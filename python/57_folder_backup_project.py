94f4062a8b61b87746976ab69e87d0de02efa018
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-30 14:50:34 +0530

    Figuring out what files should be used based on what files already exist

--- python/57_folder_backup_project.py
+++ python/57_folder_backup_project.py
@@ -7,4 +7,15 @@ import os
 
 # Backup the entire contents of specific folder into a ZIP file
 def backupToZip(folder):
+    target_folder = os.path.abspath(folder)
+
+    # Figure out the filename that should be used based on what files already exist
+    file_number = 1
+    while True:
+        zip_file_name = os.path.basename(
+            folder) + "_" + str(file_number) + ".zip"
+        if not os.path.exists(zip_file_name):
+            break
+        file_number += 1
+
     print("Folder backed up")
