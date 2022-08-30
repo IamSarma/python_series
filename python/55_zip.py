06f1079b8f384ee03a1789dcb2a8c7644f932298
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-30 11:12:29 +0530

    Getting file size and compress size

--- python/55_zip.py
+++ python/55_zip.py
@@ -9,6 +9,7 @@ sample_zip = zipfile.ZipFile(home_path / "test.zip")
 # print(sample_zip.namelist())
 sample_zip_info = sample_zip.getinfo("test.txt")
 print(sample_zip_info.file_size)
+print(sample_zip_info.compress_size)
 
 # Extracting from zip file(s)
 
