8d64ba8aed8999c2f47996af623eff63572eeafa
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-31 20:39:48 +0530

    Imported required module(s) and created base functionality

--- python/58_selective_copy.py
+++ python/58_selective_copy.py
@@ -3,7 +3,13 @@
 # with given file extension and copy these files from whatever location
 # they are into a new folder
 import os
+from pathlib import Path
 
 
-def copySpecificFiles(folder):
-    return True
+def copySpecificFiles(folder, file_extension):
+    base_folder = os.path.abspath(folder)
+    target_folder = Path.cwd() / "copy_here"
+
+
+copySpecificFiles(
+    "C:\\Users\\MB\\Documents\\python_series\\search_here", ".txt")
