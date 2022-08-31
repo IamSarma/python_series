c34a0ac4de9e1114a63a8d7ca2ec805e60682994
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-31 20:34:50 +0530

    Program that walks through given folder and copies the files with specific extension to target folder

---
+++ python/58_selective_copy.py
@@ -0,0 +1,9 @@
+# Selective copy
+# Program that walks through given folder tree and searched for files
+# with given file extension and copy these files from whatever location
+# they are into a new folder
+import os
+
+
+def copySpecificFiles(folder):
+    return True
