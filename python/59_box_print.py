b5c71f2d78026e27f85e8f45fdd569412cb804ac
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-31 21:36:04 +0530

    Raised exception if width is not greater than 2

--- python/59_box_print.py
+++ python/59_box_print.py
@@ -1,3 +1,5 @@
 def boxPrint(symbol, width, height):
     if len(symbol) != 1:
         raise Exception("Symbol must be a single character string")
+    if width <= 2:
+        raise Exception("Width must be greater than 2")
