23deea73c850de0443fb1da33b76c86f666abbe1
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-08-31 20:43:20 +0530

    Walking through directory tree using os.walk()

--- python/58_selective_copy.py
+++ python/58_selective_copy.py
@@ -6,9 +6,15 @@ import os
 from pathlib import Path
 
 
+# Function to search and copy file(s) with user provided file extension
 def copySpecificFiles(folder, file_extension):
     base_folder = os.path.abspath(folder)
     target_folder = Path.cwd() / "copy_here"
+
+    # Walk through the user provided directory and find the file(s)
+    for folder_name, sub_folders, file_names in os.walk(folder):
+        print(
+            f"Searching for file(s) with extension {file_extension} in {folder_name}")
 
 
 copySpecificFiles(
