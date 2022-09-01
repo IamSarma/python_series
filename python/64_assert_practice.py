71d33f7bc73f7277811554b93566dfc4ebf9bcc4
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 19:44:21 +0530

    Assertion practice - 2

--- python/64_assert_practice.py
+++ python/64_assert_practice.py
@@ -1,3 +1,10 @@
 # Assert statement that triggers an AssertionError if variable is an integer less than 10
-final_score = 9
-assert int(final_score) >= 10
+# final_score = 9
+# assert int(final_score) >= 10
+
+
+# Assert statement that triggers an AssertionError if two variables contains strings
+# that are same as each other, even if their cases are different
+message_1 = "Hello"
+message_2 = "HeLlO"
+assert message_1.lower() != message_2.lower()
