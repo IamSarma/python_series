3c6f14a1c2c99918475129f198043fb6662c2298
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 12:18:56 +0530

    Debugging a number adding program

---
+++ python/63_buggy_adding_program.py
@@ -0,0 +1,9 @@
+# Debugging a number adding program
+print("Enter the first number to add:")
+first_num = input()
+print("Enter the second number to add:")
+second_num = input()
+print("Enter the third number to add:")
+third_num = input()
+
+print("The sum is " + first_num + second_num + third_num)
