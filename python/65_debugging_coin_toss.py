fde0381dcef9a2d95053ab516973d0435cc15ccb
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 20:15:12 +0530

    Debugging coin toss program done

--- python/65_debugging_coin_toss.py
+++ python/65_debugging_coin_toss.py
@@ -1,16 +1,20 @@
 # Debugging coin toss program
 import random
+
 guess = ''
+
 while guess not in ('heads', 'tails'):
     print('Guess the coin toss! Enter heads or tails:')
     guess = input()
     toss = random.randint(0, 1)  # 0 is tails, 1 is heads
-    if toss == guess:
+    coin_flip = "heads" if toss == 1 else "tails"
+
+    if guess == coin_flip:
         print('You got it!')
     else:
         print('Nope! Guess again!')
-        guesss = input()
-        if toss == guess:
+        guess = input()
+        if guess == coin_flip:
             print('You got it!')
         else:
             print('Nope. You are really bad at this game.')
