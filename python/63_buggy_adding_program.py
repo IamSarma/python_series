545d112961220453f5da718bd75d34aaf479062e
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 12:19:41 +0530

    Using breakpoints

--- python/63_buggy_adding_program.py
+++ python/63_buggy_adding_program.py
@@ -1,9 +1,22 @@
-# Debugging a number adding program
-print("Enter the first number to add:")
-first_num = input()
-print("Enter the second number to add:")
-second_num = input()
-print("Enter the third number to add:")
-third_num = input()
+import random
 
-print("The sum is " + first_num + second_num + third_num)
+# Debugging a number adding program
+# print("Enter the first number to add:")
+# first_num = input()
+# print("Enter the second number to add:")
+# second_num = input()
+# print("Enter the third number to add:")
+# third_num = input()
+
+# print("The sum is " + first_num + second_num + third_num)
+
+
+# Breakpoints
+heads_count = 0
+for i in range(1, 1001):
+    if random.randint(0, 1) == 1:
+        heads_count += 1
+    if i == 500:
+        print("Halfway done!")
+
+print(f"Heads came up {str(heads_count)} times")
