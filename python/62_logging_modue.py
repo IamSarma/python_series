9e0cc74ae8db2b9399acfc93a364e879d6c71ff9
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 11:43:13 +0530

    Identified bug using logging and fixed the code to display correct result

--- python/62_logging_modue.py
+++ python/62_logging_modue.py
@@ -8,7 +8,7 @@ logging.debug("Start of program")
 def factorial(n):
     logging.debug(f"Start of factorial({n})")
     total = 1
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         total *= i
         logging.debug(f"i is {str(i)}, total is {str(total)}")
     logging.debug(f"End of factorial({n})")
