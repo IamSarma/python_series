31e142f68b154313d13f36adab95b15ea5686c40
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 21:45:48 +0530

    Getting address from command line

--- python/66_map_it.py
+++ python/66_map_it.py
@@ -1,2 +1,11 @@
+#! python3
+# map_it.py
+# Launches a map in the browser using an address from the command line or clipboard
+
 import webbrowser
-webbrowser.open("https://www.inventwithpython.com/")
+import sys
+
+# webbrowser.open("https://www.inventwithpython.com/")
+# Get address from command line
+if len(sys.argv) > 1:
+    web_address = " ".join(sys.argv[1:])
