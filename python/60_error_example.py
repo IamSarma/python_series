740902df74b2afcdacea7d063f7ec4ce012ca6e7
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 11:00:01 +0530

    Using traceback module and store traceback info in a text file

--- python/60_error_example.py
+++ python/60_error_example.py
@@ -1,10 +1,20 @@
 # Traceback example
-def callTest():
-    test()
+import traceback
+
+# def callTest():
+#     test()
 
 
-def test():
+# def test():
+#     raise Exception("This is the error message")
+
+
+# callTest()
+
+try:
     raise Exception("This is the error message")
-
-
-callTest()
+except:
+    error_file = open("error_info.txt", "w")
+    error_file.write(traceback.format_exc())
+    error_file.close()
+    print("The traceback info was written to error_info.txt")
