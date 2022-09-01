e551024a490fe7620d03afac6724e217f4d4c650
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 22:37:30 +0530

    Saving downloaded files to the hard drive

--- python/67_requests.py
+++ python/67_requests.py
@@ -10,10 +10,21 @@ import requests
 
 
 # Checking for errors
-res = requests.get(
-    "https://automatetheboringstuff.com/page_that_does_not_exist")
+# res = requests.get(
+#     "https://automatetheboringstuff.com/page_that_does_not_exist")
 
-try:
-    res.raise_for_status()
-except Exception as exc:
-    print(f"There was a problem: {exc}")
+# try:
+#     res.raise_for_status()
+# except Exception as exc:
+#     print(f"There was a problem: {exc}")
+
+
+# Saving downloaded files to the hard drive
+res = requests.get("https://automatetheboringstuff.com/files/rj.txt")
+res.raise_for_status()
+play_file = open("Romeo_And_Juliet.txt", "wb")
+
+for chunk in res.iter_content(100000):
+    play_file.write(chunk)
+
+play_file.close()
