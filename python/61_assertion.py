41a5f346cba02e6365cb2c3b209963b9a408a85e
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 11:09:14 +0530

    Simulating failure scenario to test assert statement result

--- python/61_assertion.py
+++ python/61_assertion.py
@@ -1,5 +1,11 @@
 # Usage of assert statement
 ages = [26, 57, 92, 54, 22, 15, 17, 80, 47, 73]
-ages.sort()
+# ages.sort()
+# print(ages)
+# assert ages[0] <= ages[-1]
+
+
+# Simulating failure scenario
+ages.reverse()
 print(ages)
 assert ages[0] <= ages[-1]
