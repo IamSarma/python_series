a2f77e7818f2306611ffd8e11dd13630fb1679e6
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 22:09:22 +0530

    Checking for errors using raise_for_status()

--- python/67_requests.py
+++ python/67_requests.py
@@ -1,9 +1,15 @@
 import requests
 
-res = requests.get("https://automatetheboringstuff.com/files/rj.txt")
-res.status_code = requests.codes.ok
+# res = requests.get("https://automatetheboringstuff.com/files/rj.txt")
+# res.status_code = requests.codes.ok
 
-print(type(res))
-print(res.status_code)
-print(len(res.text))
-print(res.text[:252])
+# print(type(res))
+# print(res.status_code)
+# print(len(res.text))
+# print(res.text[:252])
+
+
+# Checking for errors
+res = requests.get(
+    "https://automatetheboringstuff.com/page_that_does_not_exist")
+res.raise_for_status()
