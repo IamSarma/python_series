e0a540dfb12ab45989ebfd2cd73bbb0d350748f0
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-01 11:52:17 +0530

    Logging - debug level

--- python/62_logging_modue.py
+++ python/62_logging_modue.py
@@ -1,19 +1,23 @@
 import logging
-# logging.disable(logging.CRITICAL)
+# # logging.disable(logging.CRITICAL)
 logging.basicConfig(level=logging.DEBUG,
                     format="%(asctime)s - %(levelname)s - %(message)s")
-logging.debug("Start of program")
+# logging.debug("Start of program")
 
 
-def factorial(n):
-    logging.debug(f"Start of factorial({n})")
-    total = 1
-    for i in range(1, n + 1):
-        total *= i
-        logging.debug(f"i is {str(i)}, total is {str(total)}")
-    logging.debug(f"End of factorial({n})")
-    return total
+# def factorial(n):
+#     logging.debug(f"Start of factorial({n})")
+#     total = 1
+#     for i in range(1, n + 1):
+#         total *= i
+#         logging.debug(f"i is {str(i)}, total is {str(total)}")
+#     logging.debug(f"End of factorial({n})")
+#     return total
 
 
-print(factorial(5))
-logging.debug("End of program")
+# print(factorial(5))
+# logging.debug("End of program")
+
+
+# Different level(s) of logging message(s)
+logging.debug("Some debugging details")
