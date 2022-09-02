eecccbbf7e5c87b54c03c5330b1dc9d059b436ad
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-02 21:13:33 +0530

    Using example html file for creating a beautiful soup object

--- python/68_bs4.py
+++ python/68_bs4.py
@@ -2,7 +2,13 @@ import requests
 import bs4
 
 # Creating a beautiful soup object from HTML
-res = requests.get("https://nostarch.com")
-res.raise_for_status()
-no_starch_soup = bs4.BeautifulSoup(res.text, "html.parser")
-print(type(no_starch_soup))
+# res = requests.get("https://nostarch.com")
+# res.raise_for_status()
+# no_starch_soup = bs4.BeautifulSoup(res.text, "html.parser")
+# print(type(no_starch_soup))
+
+
+# Using example HTML file
+example_file = open("example.html")
+example_soup = bs4.BeautifulSoup(example_file, "html.parser")
+print(type(example_soup))
