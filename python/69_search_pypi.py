c17f5969a8f1f65eae8e08e7b50d60de83c1c9eb
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-02 22:32:08 +0530

    Open several search results

---
+++ python/69_search_pypi.py
@@ -0,0 +1,18 @@
+#! python3
+# search_pypi.py
+# Open several search results
+import requests
+import sys
+import webbrowser
+import bs4
+
+# Display text while downloading the search result page
+print("Searching...")
+res = requests.get(
+    "https://google.com/search?q=" "https://pypi.org/search/?q=" + " ".join(sys.argv[1:]))
+res.raise_for_status()
+
+
+# Retrieve top search result links
+
+# Open a browser tab for each result
