73e6004d074e1a24bfefd47c7b36b6bbd166ccb8
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-03 14:05:49 +0530

    Modified imported modules and logic

--- python/72_image_site_downloader.py
+++ python/72_image_site_downloader.py
@@ -1,13 +1,13 @@
 # Program that goes to a photo-sharing site
 # Searches for categories of photos
 # Downloads all the resulting images
-from selenium import webdriver
 import sys
 import os
+import requests
+import bs4
 
 # Create a local folder to save the downloaded photos
 os.makedirs("photos", exist_ok=True)
 
-# Open the site and search for the category
-browser = webdriver.Firefox()
-browser.get("https://unsplash.com/s/photos/" + " ".join(sys.argv[1:]))
+# Generate target url
+url = "https://unsplash.com/s/photos/" + " ".join(sys.argv[1:])
