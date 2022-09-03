88d18a0dddd9ae5fd191e00b4cb194a5b51df37e
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-03 11:34:52 +0530

    Finding an element with class name

--- python/71_selenium.py
+++ python/71_selenium.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-browser = webdriver.Firefox(
-    executable_path=r"C:\Users\MB\Downloads\gecko\geckodriver.exe")
-
-browser.get("https://inventwithpython.com")
+# Finding an element with class name
+browser = webdriver.Firefox()
+browser.get("https://www.google.com")
+try:
+    class_elem = browser.find_element(By.CLASS_NAME, "lnXdpd")
+    print(f"Found <{class_elem.tag_name}> element with that class name!")
+except:
+    print("Was not able to find an element with that name")
