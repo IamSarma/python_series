7816cc21b1f73b3c2d6763fc45e53f574ccbed4b
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-03 11:56:31 +0530

    Performing click operation

--- python/71_selenium.py
+++ python/71_selenium.py
@@ -3,9 +3,17 @@ from selenium.webdriver.common.by import By
 
 # Finding an element with class name
 browser = webdriver.Firefox()
-browser.get("https://www.google.com")
-try:
-    class_elem = browser.find_element(By.CLASS_NAME, "lnXdpd")
-    print(f"Found <{class_elem.tag_name}> element with that class name!")
-except:
-    print("Was not able to find an element with that name")
+# browser.get("https://www.google.com")
+# browser.maximize_window()
+# try:
+#     class_elem = browser.find_element(By.CLASS_NAME, "lnXdpd")
+#     print(f"Found <{class_elem.tag_name}> element with that class name!")
+# except:
+#     print("Was not able to find an element with that name")
+
+
+# Performing click operation
+browser.get("https://www.python.org")
+link_elem = browser.find_element(By.ID, "about")
+link_elem.location_once_scrolled_into_view
+link_elem.click()
