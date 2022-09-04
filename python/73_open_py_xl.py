69d3b782de48639fcd4c0f4c268e0f11bf04a303
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-04 10:34:36 +0530

    Getting sheet(s) from the workbook

--- python/73_open_py_xl.py
+++ python/73_open_py_xl.py
@@ -2,4 +2,14 @@ import openpyxl
 
 # Opening Excel file
 wb = openpyxl.load_workbook("example.xlsx")
-print(type(wb))
+# print(type(wb))
+
+
+# Getting sheets from the workbook
+print(wb.sheetnames)
+sheet = wb["Sheet3"]
+print(sheet)
+print(type(sheet))
+print(sheet.title)
+active_sheet = wb.active
+print(active_sheet)
