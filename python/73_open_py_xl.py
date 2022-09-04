3c97b2dcc89ead1de12aed85ed5e693f8bfb4f28
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-04 10:39:32 +0530

    Getting cells from the sheets

--- python/73_open_py_xl.py
+++ python/73_open_py_xl.py
@@ -6,10 +6,16 @@ wb = openpyxl.load_workbook("example.xlsx")
 
 
 # Getting sheets from the workbook
-print(wb.sheetnames)
-sheet = wb["Sheet3"]
-print(sheet)
-print(type(sheet))
-print(sheet.title)
-active_sheet = wb.active
-print(active_sheet)
+# print(wb.sheetnames)
+# sheet = wb["Sheet3"]
+# print(sheet)
+# print(type(sheet))
+# print(sheet.title)
+# active_sheet = wb.active
+# print(active_sheet)
+
+
+# Getting cells from the sheets
+sheet = wb["Sheet1"]
+print(sheet["A1"])
+print(sheet["A1"].value)
