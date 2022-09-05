11dfed68ff0a6947e4979fa51b40d2a8ee859b68
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-05 20:12:21 +0530

    Saving Excel workbook

--- python/75_creating_and_writing_excel.py
+++ python/75_creating_and_writing_excel.py
@@ -1,10 +1,17 @@
 import openpyxl
 
-# Creating and saving Excel file(s)
+# Creating Excel workbook
+# wb = openpyxl.Workbook()
+# print(wb.sheetnames)
+# sheet = wb.active
+# print(sheet.title)
+# sheet.title = "master_data"
+# print(sheet.title)
+# print(wb.sheetnames)
+
+
+# Saving Excel workbook
 wb = openpyxl.Workbook()
-print(wb.sheetnames)
 sheet = wb.active
-print(sheet.title)
 sheet.title = "master_data"
-print(sheet.title)
-print(wb.sheetnames)
+wb.save("created_with_python.xlsx")
