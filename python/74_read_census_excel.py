44d797762fd25b257a3922fbdbdcbb37b9ae15f3
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-05 08:51:46 +0530

    Opening target workbook and specifying the target worksheet

--- python/74_read_census_excel.py
+++ python/74_read_census_excel.py
@@ -3,3 +3,9 @@
 # Tabulates population and number of census tracts for each county
 import openpyxl
 import pprint
+
+# Read the spreadsheet data
+print("Opening workbook...")
+wb = openpyxl.load_workbook("censuspopdata.xlsx")
+sheet = wb["Population by Census Tract"]
+county_data = {}
