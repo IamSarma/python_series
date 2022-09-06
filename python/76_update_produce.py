97dfe520846c178a6c481da9687b070902c600e7
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-06 08:27:41 +0530

    Loop through the rows and update the prices

--- python/76_update_produce.py
+++ python/76_update_produce.py
@@ -6,9 +6,19 @@ import openpyxl
 wb = openpyxl.load_workbook("produce_sales.xlsx")
 sheet = wb["Sheet"]
 
+
 # The produce types and their updated prices
 PRICE_UPDATES = {
     "Garlic": 3.07,
     "Celery": 1.19,
     "Lemon": 1.27
 }
+
+
+# Loop through the rows and update the prices
+for row_num in range(2, sheet.max_row):
+    produce_name = sheet.cell(row=row_num, column=1).value
+    if produce_name in PRICE_UPDATES:
+        sheet.cell(row=row_num, column=2).value = PRICE_UPDATES[produce_name]
+
+wb.save("updated_produce_sales.xlsx")
