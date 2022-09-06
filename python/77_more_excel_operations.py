0dbadb639de57986c42725f9ad9020f6d5f3eebd
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-06 09:04:45 +0530

    Font object(s) - 1

--- python/77_more_excel_operations.py
+++ python/77_more_excel_operations.py
@@ -4,7 +4,13 @@ from openpyxl.styles import Font
 # Setting the font style of cells
 wb = openpyxl.Workbook()
 sheet = wb["Sheet"]
-italic_24_font = Font(size=24, italic=True)
-sheet["A1"] = "Python is awesome!!!"
-sheet["A1"].font = italic_24_font
-wb.save("styles.xlsx")
+# italic_24_font = Font(size=24, italic=True)
+# sheet["A1"] = "Python is awesome!!!"
+# sheet["A1"].font = italic_24_font
+# wb.save("styles.xlsx")
+
+
+# Font objecct(s)
+font_obj_1 = Font(name="Times New Roman", bold=True)
+sheet["A1"] = "Bold Times New Roman"
+sheet["A1"].font = font_obj_1
