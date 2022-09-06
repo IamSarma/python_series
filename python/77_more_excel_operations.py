fb4703927d903222444f8a4a52f7d4b881590d73
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-06 21:23:56 +0530

    Creating chart(s) - Bar chart

--- python/77_more_excel_operations.py
+++ python/77_more_excel_operations.py
@@ -53,7 +53,22 @@ sheet = wb["Sheet"]
 
 
 # Freeze panes
-wb = openpyxl.load_workbook("produce_sales.xlsx")
-sheet = wb.active
-sheet.freeze_panes = "A2"
-wb.save("freeze_example.xlsx")
+# wb = openpyxl.load_workbook("produce_sales.xlsx")
+# sheet = wb.active
+# sheet.freeze_panes = "A2"
+# wb.save("freeze_example.xlsx")
+
+
+# Charts
+for i in range(1, 11):
+    sheet["A" + str(i)] = i
+
+ref_obj = openpyxl.chart.Reference(
+    sheet, min_col=1, min_row=1, max_col=1, max_row=10)
+series_obj = openpyxl.chart.Series(ref_obj, title="First series")
+chart_obj = openpyxl.chart.BarChart()
+chart_obj.title = "My Chart"
+chart_obj.append(series_obj)
+
+sheet.add_chart(chart_obj, "C5")
+wb.save("sample_chart.xlsx")
