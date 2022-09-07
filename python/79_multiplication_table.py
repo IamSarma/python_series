694ac93f117a082bad3fcd9e39e99849941b94d8
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-07 19:58:58 +0530

    Generate table headers row and column wise

--- python/79_multiplication_table.py
+++ python/79_multiplication_table.py
@@ -11,5 +11,10 @@ wb = openpyxl.Workbook()
 # Create worksheet object
 sheet = wb["Sheet"]
 
+# Generate table headers row and column wise
+for i in range(2, 8):
+    sheet.cell(row=i, column=1).value = i - 1
+    sheet.cell(row=1, column=i).value = i - 1
+
 # Save wokbook
 wb.save("multiplication_table.xlsx")
