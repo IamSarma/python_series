fde6e9a23b6e395213b8486844df647094620b57
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-07 21:02:22 +0530

    Create worksheet object

--- python/81_cell_inverter.py
+++ python/81_cell_inverter.py
@@ -4,5 +4,8 @@ import openpyxl
 # Create workbook object
 wb = openpyxl.load_workbook("invert_rows_columns.xlsx")
 
+# Create worksheet object
+sheet = wb["Sheet1"]
+
 # Save workbook
 wb.save("invert_rows_columns.xlsx")
