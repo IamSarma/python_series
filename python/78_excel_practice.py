19425a0e0a1cd5eafe5fde4a7379b116df068e98
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-07 09:40:55 +0530

    openpyxl practice - Get column letter/string name from integer index

--- python/78_excel_practice.py
+++ python/78_excel_practice.py
@@ -1,6 +1,6 @@
 # Excel practice question(s)
 import openpyxl
-from openpyxl.utils import column_index_from_string
+from openpyxl.utils import column_index_from_string, get_column_letter
 
 
 # Open/load workbook
@@ -40,6 +40,11 @@ print(f"The data type of sheet.max_column is: {type(sheet.max_column)}")
 # Get column index as integer value
 col_int_index = column_index_from_string("C")
 print(f"The index of column C is: {col_int_index}")
+
+# Get column letter/string name from integer index
+col_index = 8
+col_str_letter = get_column_letter(col_index)
+print(f"The string letter for column index {col_index} is: {col_str_letter}")
 
 # Save workbook
 wb.save("excel_practice.xlsx")
