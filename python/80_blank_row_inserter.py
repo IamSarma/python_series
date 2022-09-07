2d960d761a6edee0e7f50e964866d9b3aa2ee80d
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-07 20:24:25 +0530

    Creating worksheet object

--- python/80_blank_row_inserter.py
+++ python/80_blank_row_inserter.py
@@ -6,5 +6,8 @@ import sys
 # Create workbook object
 wb = openpyxl.load_workbook("insert_blank_row.xlsx")
 
+# Create worksheet object
+sheet = wb["Sheet1"]
+
 # Save workbook
 wb.save("insert_blank_row.xlsx")
