43aa60bd84dd13b8aa19f18fef48f12702b458b6
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-07 19:55:41 +0530

    Create worksheet object

--- python/79_multiplication_table.py
+++ python/79_multiplication_table.py
@@ -1,9 +1,15 @@
 # Program to generate multiplication table basis the user input
 # and update the same in Excel
 import openpyxl
+import sys
+
+table_data = []
 
 # Create workbook
 wb = openpyxl.Workbook()
+
+# Create worksheet object
+sheet = wb["Sheet"]
 
 # Save wokbook
 wb.save("multiplication_table.xlsx")
