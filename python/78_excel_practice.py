361869d207e06577c6401f2ea19fff75e8baffe8
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-07 09:31:18 +0530

    openpyxl practice - Retrieve cell's row and column as integers

--- python/78_excel_practice.py
+++ python/78_excel_practice.py
@@ -22,5 +22,11 @@ sheet["A1"] = "Python"
 # Get cell value
 print(sheet["A1"].value)
 
+# Retrieve cell's row and column as integers
+c = sheet["A1"]
+print("Selected cell is A1")
+print(f"Row number: {c.row}")
+print(f"Column number: {c.column}")
+
 # Save workbook
 wb.save("excel_practice.xlsx")
