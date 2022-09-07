f4edbe8564820f81c3e442411447ee8e530635cf
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-07 21:31:14 +0530

    Write inverted data to target sheet

--- python/81_cell_inverter.py
+++ python/81_cell_inverter.py
@@ -9,6 +9,7 @@ wb = openpyxl.load_workbook("invert_rows_columns.xlsx")
 
 # Create worksheet object
 sheet = wb["Sheet1"]
+target_sheet = wb["Sheet2"]
 
 # Read data from spreadsheet
 for i in range(1, sheet.max_column+1):
@@ -16,6 +17,11 @@ for i in range(1, sheet.max_column+1):
         items_data.append(sheet.cell(row=j, column=i).value)
     cells_data.append(list(items_data))
     items_data = []
+
+# Write inverted data to spreadsheet
+for i in range(0, len(cells_data)):
+    for j in range(0, len(cells_data[i])):
+        target_sheet.cell(row=i+1, column=j+1).value = cells_data[i][j]
 
 # Save workbook
 wb.save("invert_rows_columns.xlsx")
