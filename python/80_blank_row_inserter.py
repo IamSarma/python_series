6b42fc30e6d52e8c2beac0e1e61411871c2cef4e
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-07 20:27:11 +0530

    Taking user input for row number

--- python/80_blank_row_inserter.py
+++ python/80_blank_row_inserter.py
@@ -9,5 +9,8 @@ wb = openpyxl.load_workbook("insert_blank_row.xlsx")
 # Create worksheet object
 sheet = wb["Sheet1"]
 
+# User input - row number
+row_number = sys.argv[1]
+
 # Save workbook
 wb.save("insert_blank_row.xlsx")
