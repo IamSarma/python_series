23b5fbb6b67181e42a04b8e48e8a2265140fe538
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-08 08:17:27 +0530

    Imported os module and looping through text file(s) in the target folder

--- python/82_text_files_to_excel.py
+++ python/82_text_files_to_excel.py
@@ -1,12 +1,21 @@
 # Program to read the contents of several text file(s) and
 # insert each text file content into separate column(s)
 import openpyxl
+import os
+
+# Folder path containing text file(s)
+target_folder = os.getcwd() + "/text_files"
+
+# Loop through target folder to read text file contents
+for file in os.listdir(target_folder):
+    if file.endswith(".txt"):
+        print(file)
 
 # Create workbook object
-wb = openpyxl.load_workbook("text_to_excel.xlsx")
+# wb = openpyxl.load_workbook("text_to_excel.xlsx")
 
 # Create worksheet object
-sheet = wb["Sheet1"]
+# sheet = wb["Sheet1"]
 
 # Save workbook
-wb.save("text_to_excel.xlsx")
+# wb.save("text_to_excel.xlsx")
