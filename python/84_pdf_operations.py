fa4074cc33aff27f9b250caa7df4d74eeafbc1a1
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-09 20:56:09 +0530

    Overlaying pages - merging watermark added PDF to different PDF file

--- python/84_pdf_operations.py
+++ python/84_pdf_operations.py
@@ -42,15 +42,27 @@ import PyPDF2
 
 
 # Rotating pages
+# minutes_file = open("meetingminutes.pdf", "rb")
+# pdf_reader = PyPDF2.PdfFileReader(minutes_file)
+# page = pdf_reader.getPage(0)
+# page.rotateClockwise(90)
+
+# pdf_writer = PyPDF2.PdfFileWriter()
+# pdf_writer.addPage(page)
+
+# result_pdf_file = open("rotated_page.pdf", "wb")
+# pdf_writer.write(result_pdf_file)
+# result_pdf_file.close()
+# minutes_file.close()
+
+
+# Overlaying pages
 minutes_file = open("meetingminutes.pdf", "rb")
 pdf_reader = PyPDF2.PdfFileReader(minutes_file)
-page = pdf_reader.getPage(0)
-page.rotateClockwise(90)
+minutes_first_page = pdf_reader.getPage(0)
+
+pdf_watermark_reader = PyPDF2.PdfFileReader(open("watermark.pdf", "rb"))
+minutes_first_page.mergePage(pdf_watermark_reader.getPage(0))
 
 pdf_writer = PyPDF2.PdfFileWriter()
-pdf_writer.addPage(page)
-
-result_pdf_file = open("rotated_page.pdf", "wb")
-pdf_writer.write(result_pdf_file)
-result_pdf_file.close()
-minutes_file.close()
+pdf_writer.addPage(minutes_first_page)
