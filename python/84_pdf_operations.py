ab984bba4589443d2790e413fd02bff6d78b5463
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-09 09:15:36 +0530

    Decrypting PDFs - checking if given PDF is encrypted

--- python/84_pdf_operations.py
+++ python/84_pdf_operations.py
@@ -1,9 +1,15 @@
 import PyPDF2
 
-pdf_file_obj = open("meetingminutes.pdf", "rb")
-pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
-print(pdf_reader.numPages)
+# Extracting text from PDF
+# pdf_file_obj = open("meetingminutes.pdf", "rb")
+# pdf_reader = PyPDF2.PdfFileReader(pdf_file_obj)
+# print(pdf_reader.numPages)
 
-page_obj = pdf_reader.getPage(0)
-print(page_obj.extractText())
-pdf_file_obj.close()
+# page_obj = pdf_reader.getPage(0)
+# print(page_obj.extractText())
+# pdf_file_obj.close()
+
+
+# Decrypting PDFs
+pdf_reader = PyPDF2.PdfFileReader(open("encrypted.pdf", "rb"))
+print(pdf_reader.isEncrypted)
