27b511182b176a358bfdfc3978af16e7791fed30
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-09 20:46:15 +0530

    Rotating PDF clockwise by 90 degress

--- python/84_pdf_operations.py
+++ python/84_pdf_operations.py
@@ -19,23 +19,30 @@ import PyPDF2
 
 
 # Copying pages
-pdf1_file = open("meetingminutes.pdf", "rb")
-pdf2_file = open("meetingminutes2.pdf", "rb")
+# pdf1_file = open("meetingminutes.pdf", "rb")
+# pdf2_file = open("meetingminutes2.pdf", "rb")
 
-pdf1_reader = PyPDF2.PdfFileReader(pdf1_file)
-pdf2_reader = PyPDF2.PdfFileReader(pdf2_file)
-pdf_writer = PyPDF2.PdfFileWriter()
+# pdf1_reader = PyPDF2.PdfFileReader(pdf1_file)
+# pdf2_reader = PyPDF2.PdfFileReader(pdf2_file)
+# pdf_writer = PyPDF2.PdfFileWriter()
 
-for page_num in range(pdf1_reader.numPages):
-    page_obj = pdf1_reader.getPage(page_num)
-    pdf_writer.addPage(page_obj)
+# for page_num in range(pdf1_reader.numPages):
+#     page_obj = pdf1_reader.getPage(page_num)
+#     pdf_writer.addPage(page_obj)
 
-for page_num in range(pdf1_reader.numPages):
-    page_obj = pdf2_reader.getPage(page_num)
-    pdf_writer.addPage(page_obj)
+# for page_num in range(pdf1_reader.numPages):
+#     page_obj = pdf2_reader.getPage(page_num)
+#     pdf_writer.addPage(page_obj)
 
-pdf_output_file = open("combinedminutes.pdf", "wb")
-pdf_writer.write(pdf_output_file)
-pdf_output_file.close()
-pdf1_file.close()
-pdf2_file.close()
+# pdf_output_file = open("combinedminutes.pdf", "wb")
+# pdf_writer.write(pdf_output_file)
+# pdf_output_file.close()
+# pdf1_file.close()
+# pdf2_file.close()
+
+
+# Rotating pages
+minutes_file = open("meetingminutes.pdf", "rb")
+pdf_reader = PyPDF2.PdfFileReader(minutes_file)
+page = pdf_reader.getPage(0)
+page.rotateCloclwise(90)
