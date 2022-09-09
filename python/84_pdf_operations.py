1b479fe623d395264d5e6cb3f3d44475655d3c17
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-09 21:03:55 +0530

    Encrypting PDFs - created PDF reader and writer object(s)

--- python/84_pdf_operations.py
+++ python/84_pdf_operations.py
@@ -57,21 +57,24 @@ import PyPDF2
 
 
 # Overlaying pages
-minutes_file = open("meetingminutes.pdf", "rb")
-pdf_reader = PyPDF2.PdfFileReader(minutes_file)
-minutes_first_page = pdf_reader.getPage(0)
+# minutes_file = open("meetingminutes.pdf", "rb")
+# pdf_reader = PyPDF2.PdfFileReader(minutes_file)
+# minutes_first_page = pdf_reader.getPage(0)
 
-pdf_watermark_reader = PyPDF2.PdfFileReader(open("watermark.pdf", "rb"))
-minutes_first_page.mergePage(pdf_watermark_reader.getPage(0))
+# pdf_watermark_reader = PyPDF2.PdfFileReader(open("watermark.pdf", "rb"))
+# minutes_first_page.mergePage(pdf_watermark_reader.getPage(0))
 
-pdf_writer = PyPDF2.PdfFileWriter()
-pdf_writer.addPage(minutes_first_page)
+# pdf_writer = PyPDF2.PdfFileWriter()
+# pdf_writer.addPage(minutes_first_page)
 
-for page_num in range(1, pdf_reader.numPages):
-    page_obj = pdf_reader.getPage(page_num)
-    pdf_writer.addPage(page_obj)
+# for page_num in range(1, pdf_reader.numPages):
+#     page_obj = pdf_reader.getPage(page_num)
+#     pdf_writer.addPage(page_obj)
 
-result_pdf_file = open("watermarked_cover.pdf", "wb")
-pdf_writer.write(result_pdf_file)
-result_pdf_file.close()
-minutes_file.close()
+# result_pdf_file = open("watermarked_cover.pdf", "wb")
+# pdf_writer.write(result_pdf_file)
+# result_pdf_file.close()
+# minutes_file.close()
+
+
+# Encrypting PDFs
