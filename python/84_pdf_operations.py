3262987c3a7ccb1970543e306e00bfdf6a4f851e
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-09 21:00:33 +0530

    Saving the merged watermark covered page along with rest of the pages to new PDF

--- python/84_pdf_operations.py
+++ python/84_pdf_operations.py
@@ -66,3 +66,12 @@ minutes_first_page.mergePage(pdf_watermark_reader.getPage(0))
 
 pdf_writer = PyPDF2.PdfFileWriter()
 pdf_writer.addPage(minutes_first_page)
+
+for page_num in range(1, pdf_reader.numPages):
+    page_obj = pdf_reader.getPage(page_num)
+    pdf_writer.addPage(page_obj)
+
+result_pdf_file = open("watermarked_cover.pdf", "wb")
+pdf_writer.write(result_pdf_file)
+result_pdf_file.close()
+minutes_file.close()
