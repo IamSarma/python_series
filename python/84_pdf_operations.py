bf0691727e7456b8b07f26182b67a95db107f83d
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-09 20:49:17 +0530

    Saving rotated page into new PDF file

--- python/84_pdf_operations.py
+++ python/84_pdf_operations.py
@@ -45,4 +45,12 @@ import PyPDF2
 minutes_file = open("meetingminutes.pdf", "rb")
 pdf_reader = PyPDF2.PdfFileReader(minutes_file)
 page = pdf_reader.getPage(0)
-page.rotateCloclwise(90)
+page.rotateClockwise(90)
+
+pdf_writer = PyPDF2.PdfFileWriter()
+pdf_writer.addPage(page)
+
+result_pdf_file = open("rotated_page.pdf", "wb")
+pdf_writer.write(result_pdf_file)
+result_pdf_file.close()
+minutes_file.close()
