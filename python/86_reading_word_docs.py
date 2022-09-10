d39745e315a5697af0d20bcde0bc0338841319f9
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-10 09:07:15 +0530

    Getting number of runs in the paragraphs object

--- python/86_reading_word_docs.py
+++ python/86_reading_word_docs.py
@@ -8,3 +8,7 @@ print(len(doc.paragraphs))
 # Getting text using index of the paragraphs object
 print(doc.paragraphs[0].text)
 print(doc.paragraphs[1].text)
+
+
+# Getting number of runs in the paragraphs object
+print(len(doc.paragraphs[1].runs))
