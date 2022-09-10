a94b05b5c0e26070f1ca71427fe747ff920c4ae2
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-10 09:05:41 +0530

    Getting text from paragraphs object using index

--- python/86_reading_word_docs.py
+++ python/86_reading_word_docs.py
@@ -1,4 +1,10 @@
 import docx
 
 doc = docx.Document("demo.docx")
+
+# Getting the length of the paragraphs in the document
 print(len(doc.paragraphs))
+
+# Getting text using index of the paragraphs object
+print(doc.paragraphs[0].text)
+print(doc.paragraphs[1].text)
