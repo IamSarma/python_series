9b6b1a24b3be71985610fc1ed53fa08710117c11
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-10 09:08:50 +0530

    Getting text from the runs object using index and text

--- python/86_reading_word_docs.py
+++ python/86_reading_word_docs.py
@@ -9,6 +9,12 @@ print(len(doc.paragraphs))
 print(doc.paragraphs[0].text)
 print(doc.paragraphs[1].text)
 
-
 # Getting number of runs in the paragraphs object
 print(len(doc.paragraphs[1].runs))
+
+# Getting text from the runs object using index and text
+print(doc.paragraphs[1].runs[0].text)
+print(doc.paragraphs[1].runs[1].text)
+print(doc.paragraphs[1].runs[2].text)
+print(doc.paragraphs[1].runs[3].text)
+print(doc.paragraphs[1].runs[4].text)
