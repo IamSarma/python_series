b0b64bf3b7ebf36e6811241ee54b9ddace329a48
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-11 11:14:59 +0530

    Program to convert word to PDF, created word and pdf file name(s) as required

---
+++ python/87_convert_word_to_pdf.py
@@ -0,0 +1,7 @@
+# This script runs on Windows only and Word must be installed
+import win32com.client
+import docx
+
+# Assinging file name(s) to word and pdf file(s)
+word_file_name = "word_to_pdf.docx"
+pdf_file_name = "pdf_from_word.pdf"
