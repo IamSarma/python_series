897e5654e3924aac42bffd8f973380296e8baba3
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-11 11:24:15 +0530

    Logic implemented to convert word document to PDF

--- python/87_convert_word_to_pdf.py
+++ python/87_convert_word_to_pdf.py
@@ -1,6 +1,7 @@
 # This script runs on Windows only and Word must be installed
 import win32com.client
 import docx
+import os
 
 # Assinging file name(s) to word and pdf file(s)
 word_file_name = "word_to_pdf.docx"
@@ -15,3 +16,8 @@ doc.save(word_file_name)
 # Convert word to PDF
 word_format_pdf = 17        # Word's numeric code for PDF
 word_obj = win32com.client.Dispatch("Word.Application")
+doc_obj = word_obj.Documents.Open(os.path.join(os.getcwd(), word_file_name))
+doc_obj.SaveAs(os.path.join(os.getcwd(), pdf_file_name),
+               FileFormat=word_format_pdf)
+doc_obj.Close()
+word_obj.Quit()
