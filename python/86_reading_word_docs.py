c3519c295cb2cd61be934e82dbdf249781f741c3
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-11 10:25:14 +0530

    Extract and display full text from the given word document using imported function getText() from read_docs.py

--- python/86_reading_word_docs.py
+++ python/86_reading_word_docs.py
@@ -1,20 +1,25 @@
 import docx
+import read_docx
 
 doc = docx.Document("demo.docx")
 
-# Getting the length of the paragraphs in the document
-print(len(doc.paragraphs))
+# # Getting the length of the paragraphs in the document
+# print(len(doc.paragraphs))
 
-# Getting text using index of the paragraphs object
-print(doc.paragraphs[0].text)
-print(doc.paragraphs[1].text)
+# # Getting text using index of the paragraphs object
+# print(doc.paragraphs[0].text)
+# print(doc.paragraphs[1].text)
 
-# Getting number of runs in the paragraphs object
-print(len(doc.paragraphs[1].runs))
+# # Getting number of runs in the paragraphs object
+# print(len(doc.paragraphs[1].runs))
 
-# Getting text from the runs object using index and text
-print(doc.paragraphs[1].runs[0].text)
-print(doc.paragraphs[1].runs[1].text)
-print(doc.paragraphs[1].runs[2].text)
-print(doc.paragraphs[1].runs[3].text)
-print(doc.paragraphs[1].runs[4].text)
+# # Getting text from the runs object using index and text
+# print(doc.paragraphs[1].runs[0].text)
+# print(doc.paragraphs[1].runs[1].text)
+# print(doc.paragraphs[1].runs[2].text)
+# print(doc.paragraphs[1].runs[3].text)
+# print(doc.paragraphs[1].runs[4].text)
+
+
+# Extract and display full text from the given word document
+print(read_docx.getText("demo.docx"))
