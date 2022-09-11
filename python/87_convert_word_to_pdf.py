3b6a763c9d51c42acdcbf76be98e9eea7ccfd4e1
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-11 11:16:43 +0530

    Created and saved word document with required content

--- python/87_convert_word_to_pdf.py
+++ python/87_convert_word_to_pdf.py
@@ -5,3 +5,9 @@ import docx
 # Assinging file name(s) to word and pdf file(s)
 word_file_name = "word_to_pdf.docx"
 pdf_file_name = "pdf_from_word.pdf"
+
+# Create and save word document
+doc = docx.Document()
+doc.add_paragraph(
+    "This word document is created and converted to PDF using Python 🐍🤩")
+doc.save(word_file_name)
