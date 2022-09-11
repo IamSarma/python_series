2e025d59c9d32054926a7638b326ebafbf5498a2
Sarma Akondi V N M <sarmaakondivnm@gmail.com>
2022-09-11 10:51:19 +0530

    Add multiple paragraph(s) using add_run() method

--- python/86_reading_word_docs.py
+++ python/86_reading_word_docs.py
@@ -46,3 +46,8 @@ import read_docx
 doc = docx.Document()
 doc.add_paragraph("Python is awesome!!!")
 doc.save("made_with_python.docx")
+
+para_obj1 = doc.add_paragraph("This is a second paragraph.")
+para_obj2 = doc.add_paragraph("This is yet another paragraph.")
+para_obj1.add_run(" This text is being added to the second paragraph.")
+doc.save("multiple_paragraphs.docx")
