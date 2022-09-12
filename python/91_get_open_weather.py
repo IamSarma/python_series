9c17b8b4265be04381031efb24546815b7d5e4a9
IamSarma <sarmaakondivnm@gmail.com>
2022-09-12 22:52:41 +0530

    Compute location from command line arguments

--- python/91_get_open_weather.py
+++ python/91_get_open_weather.py
@@ -6,3 +6,10 @@ import requests
 import json
 
 APPID = "YOUR_APPID_HERE"
+
+# Compute location from command line arguments
+if len(sys.argv) < 2:
+    print("Usage: get_open_weather.py city_name, two_letter_country_code")
+    sys.exit()
+
+location = " ".join(sys.argv[1:])
