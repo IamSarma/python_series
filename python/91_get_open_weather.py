d595973d7d3f9f1b8cae7f00dea01bd45efe6fc3
IamSarma <sarmaakondivnm@gmail.com>
2022-09-12 22:59:54 +0530

    Download the JSON data from OpenWeatherMap.org's API

--- python/91_get_open_weather.py
+++ python/91_get_open_weather.py
@@ -16,3 +16,5 @@ location = " ".join(sys.argv[1:])
 
 # Download the JSON data from OpenWeatherMap.org's API
 weather_url = f"http://api.openweathermap.org/data/2.5/forecast/daily?q={location}&cnt=3&APPID={APPID}"
+response = requests.get(weather_url)
+response.raise_for_status()
