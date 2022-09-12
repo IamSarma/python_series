05e5e59ccdd5936e6d2cb666063fffd326c2ab06
IamSarma <sarmaakondivnm@gmail.com>
2022-09-12 09:28:16 +0530

    Reading text file data by converting it to Python list

--- python/88_csv_module.py
+++ python/88_csv_module.py
@@ -2,4 +2,6 @@ import csv
 
 # Reader Object(s)
 text_file = open("example.csv")
-text_reader = csv.reader(text_file)
+text_file_reader = csv.reader(text_file)
+text_file_data = list(text_file_reader)
+print(text_file_data)
