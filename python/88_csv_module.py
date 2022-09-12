366e4fbb165e3ee1df82f91e1dc2d03d0178c580
IamSarma <sarmaakondivnm@gmail.com>
2022-09-12 09:41:47 +0530

    Accessing data from the generated list

--- python/88_csv_module.py
+++ python/88_csv_module.py
@@ -5,3 +5,9 @@ text_file = open("example.csv")
 text_file_reader = csv.reader(text_file)
 text_file_data = list(text_file_reader)
 print(text_file_data)
+
+# Accessing data from the generated list
+print(text_file_data[0][1])
+print(text_file_data[1][0])
+print(text_file_data[2][1])
+print(text_file_data[3][2])
