f86e398e7da0c47fd7f5443caca620ee910a6741
IamSarma <sarmaakondivnm@gmail.com>
2022-09-12 09:46:24 +0530

    Reading data from reader object(s) in a for loop

--- python/88_csv_module.py
+++ python/88_csv_module.py
@@ -3,11 +3,16 @@ import csv
 # Reader Object(s)
 text_file = open("example.csv")
 text_file_reader = csv.reader(text_file)
-text_file_data = list(text_file_reader)
-print(text_file_data)
+# text_file_data = list(text_file_reader)
+# print(text_file_data)
 
 # Accessing data from the generated list
-print(text_file_data[0][1])
-print(text_file_data[1][0])
-print(text_file_data[2][1])
-print(text_file_data[3][2])
+# print(text_file_data[0][1])
+# print(text_file_data[1][0])
+# print(text_file_data[2][1])
+# print(text_file_data[3][2])
+
+# Reading data from reader object(s) in a for loop
+# Looping over the reader object works only once, create reader object again to loop second time 
+for row in text_file_reader:
+    print(f"Row # {text_file_reader.line_num} {row}")
