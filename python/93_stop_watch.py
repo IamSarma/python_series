f0a3df3e4738b1d34458e3f37860bdc23a942379
IamSarma <sarmaakondivnm@gmail.com>
2022-09-13 22:00:57 +0530

    Start tracking the lap time(s) and display to the user

--- python/93_stop_watch.py
+++ python/93_stop_watch.py
@@ -14,3 +14,16 @@ print("Started")
 start_time = time.time()
 last_time = start_time
 lap_num = 1
+
+
+# Start tracking the lap time(s) and display to the user
+try:
+    while True:
+        input()
+        lap_time = round(time.time() - last_time, 2)
+        total_time = round(time.time() - start_time, 2)
+        print(f"Lap #{lap_num}: {total_time} {lap_time}", end="")
+        lap_num += 1
+        last_time = time.time()     # Reset the lap time
+except KeyboardInterrupt:
+    print("\nDone")
