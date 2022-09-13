3ba1e5114dc917dd71dcbb9d2f85f59884e4ab8b
IamSarma <sarmaakondivnm@gmail.com>
2022-09-13 21:26:56 +0530

    Displaying time using ctime() function

--- python/92_time_module.py
+++ python/92_time_module.py
@@ -2,20 +2,24 @@ import time
 
 # time() function returns the total seconds passed between the
 # UNIX epoch (12AM January 1 1970) and the moment time() was called 
-print(time.time())
+# print(time.time())
 
 
 # Function that calculate the product of the first 100_000 numbers
-def calcProd():
-    prod_value = 1
-    for i in range(1, 1200):
-        prod_value *= i
-    return prod_value
+# def calcProd():
+#     prod_value = 1
+#     for i in range(1, 1200):
+#         prod_value *= i
+#     return prod_value
 
-# Calculating the execution time
-start_time = time.time()
-result = calcProd()
-end_time = time.time()
+# # Calculating the execution time
+# start_time = time.time()
+# result = calcProd()
+# end_time = time.time()
 
-print(f"The result is {len(str(result))} characters long")
-print(f"Took {end_time - start_time} seconds to calculate")
+# print(f"The result is {len(str(result))} characters long")
+# print(f"Took {end_time - start_time} seconds to calculate")
+
+
+# Displaying time using ctime() function
+print(time.ctime())
