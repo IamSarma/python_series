dd0a45f80214b847537f0806757ff4f38f05ee35
IamSarma <sarmaakondivnm@gmail.com>
2022-09-13 21:55:41 +0530

    Take user input and start the lap

--- python/93_stop_watch.py
+++ python/93_stop_watch.py
@@ -2,6 +2,15 @@
 # stop_watch.py - A simple stopwatch program
 import time
 
+
 # Display the program's instructions
 print("Press ENTER to begin. Afterward, press ENTER to click the stopwatch")
 print("Press CTRL+C to quit")
+
+
+# Take user input and start the lap
+input()
+print("Started")
+start_time = time.time()
+last_time = start_time
+lap_num = 1
