cfea1dba3d31a42d7a0d06b3e21fba96c922fc2c
IamSarma <sarmaakondivnm@gmail.com>
2022-09-13 19:40:50 +0530

    Function that calculate the product of the first 100_000 numbers

--- python/92_time_module.py
+++ python/92_time_module.py
@@ -3,3 +3,11 @@ import time
 # time() function returns the total seconds passed between the
 # UNIX epoch (12AM January 1 1970) and the moment time() was called 
 print(time.time())
+
+
+# Function that calculate the product of the first 100_000 numbers
+def calcProd():
+    prod_value = 1
+    for i in range(1, 100_00):
+        prod_value *= i
+    return prod_value
