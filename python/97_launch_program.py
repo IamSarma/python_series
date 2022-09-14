0accdb5b18fa51af2edbb845a865033713a870a1
IamSarma <sarmaakondivnm@gmail.com>
2022-09-14 21:38:32 +0530

    Checking if the Paint application is still open using poll() method

--- python/97_launch_program.py
+++ python/97_launch_program.py
@@ -2,3 +2,7 @@ import subprocess
 
 # Launching Microsoft Paint
 paint_process = subprocess.Popen("C:\Program Files\WindowsApps\Microsoft.Paint_11.2206.6.0_x64__8wekyb3d8bbwe\PaintApp\mspaint.exe")
+
+
+# Checking if the Paint application is still open using poll() method
+print(paint_process.poll() == None)
