7be3fdaf5d0c8105837692918caecf615164fdf5
IamSarma <sarmaakondivnm@gmail.com>
2022-09-14 09:04:34 +0530

    Comparing datetime object(s)

--- python/94_date_time.py
+++ python/94_date_time.py
@@ -15,3 +15,14 @@ print(current_date.hour, current_date.minute, current_date.second)
 # Converting Unix epoch timestamp to a datetime object
 print(datetime.datetime.fromtimestamp(1_000_000))
 print(datetime.datetime.fromtimestamp(time.time()))
+
+
+# Comparing datetime object(s)
+halloween_2019 = datetime.datetime(2019, 10, 31, 0, 0, 0)
+newyear_2020 = datetime.datetime(2020, 1, 1, 0, 0, 0)
+dob = datetime.datetime(1987, 2, 27, 0, 0, 0)
+
+print(halloween_2019 == dob)
+print(halloween_2019 > newyear_2020)
+print(newyear_2020 > dob)
+print(newyear_2020 != halloween_2019)
