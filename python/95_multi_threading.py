2d658117d450e2605aa8e94f8347998df6fd97a8
IamSarma <sarmaakondivnm@gmail.com>
2022-09-14 19:23:21 +0530

    Passing argument(s) to the thread's target function

--- python/95_multi_threading.py
+++ python/95_multi_threading.py
@@ -2,16 +2,21 @@ import threading
 import time
 
 # Dispalying start message
-print("Start of the program")
+# print("Start of the program")
 
 # Function that sleeps for a while and displays message
-def takeANap():
-    time.sleep(5)
-    print("Wake up!!!")
+# def takeANap():
+#     time.sleep(5)
+#     print("Wake up!!!")
 
 # Creating and starting a new thread
-thread_obj = threading.Thread(target=takeANap)
-thread_obj.start()
+# thread_obj = threading.Thread(target=takeANap)
+# thread_obj.start()
 
 # Dispalying end message
-print("End of the program")
+# print("End of the program")
+
+
+# Passing argument(s) to the thread's target function
+thread_obj = threading.Thread(target=print, args=["Cats", "Dogs", "Frogs"], kwargs={"sep": " & "})
+thread_obj.start()
