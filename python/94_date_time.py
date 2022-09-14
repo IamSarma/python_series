b6572c3d26e4a983674ba19e693ac7f688f86107
IamSarma <sarmaakondivnm@gmail.com>
2022-09-14 08:54:22 +0530

    Displaying year, month, day, hour, minute and seconds

--- python/94_date_time.py
+++ python/94_date_time.py
@@ -3,3 +3,9 @@ import datetime
 
 # Displaying current date and time
 print(datetime.datetime.now())
+
+
+# Displaying year, month, day, hour, minute and seconds
+current_date = datetime.datetime.now()
+print(current_date.year, current_date.month, current_date.day)
+print(current_date.hour, current_date.minute, current_date.second)
