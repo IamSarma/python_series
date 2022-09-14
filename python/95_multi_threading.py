75329086a31efcc39694f7a2ab478754f8bf7f5f
IamSarma <sarmaakondivnm@gmail.com>
2022-09-14 19:10:05 +0530

    Creating and starting a new thread

--- python/95_multi_threading.py
+++ python/95_multi_threading.py
@@ -1,10 +1,17 @@
 import threading
 import time
 
+# Dispalying start message
 print("Start of the program")
 
+# Function that sleeps for a while and displays message
 def takeANap():
     time.sleep(5)
     print("Wake up!!!")
 
+# Creating and starting a new thread
+thread_obj = threading.Thread(target=takeANap)
+thread_obj.start()
+
+# Dispalying end message
 print("End of the program")
