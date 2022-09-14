c96eab66bd574d31d8c792731ceb4363c6e15fd3
IamSarma <sarmaakondivnm@gmail.com>
2022-09-14 21:53:39 +0530

    Passing argument(s) to Popen() method

--- python/97_launch_program.py
+++ python/97_launch_program.py
@@ -1,14 +1,19 @@
 import subprocess
 
 # Launching Microsoft Paint
-paint_process = subprocess.Popen(
-    "C:\Program Files\WindowsApps\Microsoft.Paint_11.2206.6.0_x64__8wekyb3d8bbwe\PaintApp\mspaint.exe")
+# paint_process = subprocess.Popen(
+#     "C:\Program Files\WindowsApps\Microsoft.Paint_11.2206.6.0_x64__8wekyb3d8bbwe\PaintApp\mspaint.exe")
 
 
 # Checking if the Paint application is still open using poll() method
-print(paint_process.poll() == None)
+# print(paint_process.poll() == None)
 
 
 # Block until the Paint application is closed/quit
-paint_process.wait()
-print(paint_process.poll() != None)
+# paint_process.wait()
+# print(paint_process.poll() != None)
+
+
+# Passing argument(s) to Popen() method
+subprocess.Popen([r"C:\Windows\System32\notepad.exe",
+                 r"D:\Projects\python_series\open_with_Popen.txt"])
