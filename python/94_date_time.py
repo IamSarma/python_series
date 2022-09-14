d39db03926875a8187d496a04727479f3d11a36f
IamSarma <sarmaakondivnm@gmail.com>
2022-09-14 09:00:45 +0530

    Converting Unix epoch timestamp to a datetime object

--- python/94_date_time.py
+++ python/94_date_time.py
@@ -1,4 +1,5 @@
 import datetime
+import time
 
 
 # Displaying current date and time
@@ -9,3 +10,8 @@ print(datetime.datetime.now())
 current_date = datetime.datetime.now()
 print(current_date.year, current_date.month, current_date.day)
 print(current_date.hour, current_date.minute, current_date.second)
+
+
+# Converting Unix epoch timestamp to a datetime object
+print(datetime.datetime.fromtimestamp(1_000_000))
+print(datetime.datetime.fromtimestamp(time.time()))
