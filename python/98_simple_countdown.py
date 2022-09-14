c83847dd0e8897df4a3b1023d5be38503f4e07d2
IamSarma <sarmaakondivnm@gmail.com>
2022-09-14 23:15:59 +0530

    Play sound at the end of the countdown

--- python/98_simple_countdown.py
+++ python/98_simple_countdown.py
@@ -5,8 +5,9 @@ import subprocess
 
 time_left = 10
 while time_left > 0:
-    print(time_left, end="")
+    print(f"time left: {time_left}")
     time.sleep(1)
     time_left -= 1
 
 # Play sound at the end of the countdown
+subprocess.Popen(["start", "beep.mp3"], shell=True)
