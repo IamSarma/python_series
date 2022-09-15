79db36d2efd2cfe0a2ff413d31116086404292e7
IamSarma <sarmaakondivnm@gmail.com>
2022-09-15 19:58:58 +0530

    Sending the SMTP hello message

--- python/100_smtp.py
+++ python/100_smtp.py
@@ -3,4 +3,8 @@ import smtplib
 
 # Creating SMTP object and validating the object type
 smtp_obj = smtplib.SMTP("smtp.gmail.com", 587)
-print(type(smtp_obj))
+# print(type(smtp_obj))
+
+
+# Sending the SMTP hello message
+print(smtp_obj.ehlo())
