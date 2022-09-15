7f34633be7bea4f9471b8b3969d7eb2a41f19715
IamSarma <sarmaakondivnm@gmail.com>
2022-09-15 20:02:03 +0530

    Starting TLS encryption (only if port 587 is used)

--- python/100_smtp.py
+++ python/100_smtp.py
@@ -7,4 +7,10 @@ smtp_obj = smtplib.SMTP("smtp.gmail.com", 587)
 
 
 # Sending the SMTP hello message
+# Return value 250 is "success" code in SMTP
 print(smtp_obj.ehlo())
+
+
+# Starting TLS encryption (only if port 587 is used)
+# Return value 220 indicates that server is ready
+print(smtp_obj.starttls())
