68ed362760fcc33b4cd8c9a4d055a18da98afb90
IamSarma <sarmaakondivnm@gmail.com>
2022-09-15 22:08:53 +0530

    Searching for Email - Selecting a folder

--- python/101_iampclient_pyzmail.py
+++ python/101_iampclient_pyzmail.py
@@ -1,4 +1,6 @@
 import imapclient
+import pprint
+
 
 # Connecting to an IMAP server
 imap_obj = imapclient.IMAPClient("imap.gmail.com", ssl=True)
@@ -10,3 +12,8 @@ user_email = input("Enter the full mail address: ")
 user_pwd = input("Enter the password to login: ")
 # print(imap_obj.login(user_email, user_pwd))
 imap_obj.login(user_email, user_pwd)
+
+
+# Searching for Email
+# Selecting a folder
+pprint.pprint(imap_obj.list_folders())
