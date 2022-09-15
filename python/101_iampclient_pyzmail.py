1063c4bf086703138e94f3199fb53eacacf0ed95
IamSarma <sarmaakondivnm@gmail.com>
2022-09-15 22:06:46 +0530

    Logging in to the IMAP server

--- python/101_iampclient_pyzmail.py
+++ python/101_iampclient_pyzmail.py
@@ -2,4 +2,11 @@ import imapclient
 
 # Connecting to an IMAP server
 imap_obj = imapclient.IMAPClient("imap.gmail.com", ssl=True)
-print(imap_obj)
+# print(imap_obj)
+
+
+# Logging in to the IMAP server
+user_email = input("Enter the full mail address: ")
+user_pwd = input("Enter the password to login: ")
+# print(imap_obj.login(user_email, user_pwd))
+imap_obj.login(user_email, user_pwd)
