c861df52398054cf3cd29e595cd469a6667d80bb
IamSarma <sarmaakondivnm@gmail.com>
2022-09-16 21:15:44 +0530

    Checking message detail(s)

--- python/103_sending_text_messages.py
+++ python/103_sending_text_messages.py
@@ -15,3 +15,13 @@ self_number = input("Enter your number: ")
 message_body = input("Enter message body: ")
 user_message = twilio_cli.messages.create(
     body=message_body, from_=twilio_number, to=self_number)
+
+
+# Checking message detail(s)
+print("\n\n")
+print(user_message.to)
+print(user_message.from_)
+print(user_message.body)
+print(user_message.status)
+print(user_message.date_created)
+print(user_message.date_sent)
