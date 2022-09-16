1b61d1b6ad91a928cbb9d2007943b060f5884561
IamSarma <sarmaakondivnm@gmail.com>
2022-09-16 22:07:18 +0530

    Creating image object and updated gitignore file

--- python/104_manipulating_images.py
+++ python/104_manipulating_images.py
@@ -1,8 +1,12 @@
-from PIL import ImageColor
+from PIL import ImageColor, Image
 
 
 # Getting RGBA values that user want to use
-print(ImageColor.getcolor("red", "RGBA"))
-print(ImageColor.getcolor("black", "RGBA"))
-print(ImageColor.getcolor("chocolate", "RGBA"))
-print(ImageColor.getcolor("aliceblue", "RGBA"))
+# print(ImageColor.getcolor("red", "RGBA"))
+# print(ImageColor.getcolor("black", "RGBA"))
+# print(ImageColor.getcolor("chocolate", "RGBA"))
+# print(ImageColor.getcolor("aliceblue", "RGBA"))
+
+
+# Creating image object
+self_img = Image.open("dp.png")
