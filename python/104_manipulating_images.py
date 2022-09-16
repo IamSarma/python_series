f210aa914041e03c5ec1f6a7d5cc10d7b0144271
IamSarma <sarmaakondivnm@gmail.com>
2022-09-16 22:19:11 +0530

    Working with image data type - creating new image

--- python/104_manipulating_images.py
+++ python/104_manipulating_images.py
@@ -9,17 +9,22 @@ from PIL import ImageColor, Image
 
 
 # Creating image object
-self_img = Image.open("dp.png")
+# self_img = Image.open("dp.png")
 
 
 # Working with image data type
-print(self_img.size)
-print(self_img.filename)
-print(self_img.format)
-print(self_img.format_description)
+# print(self_img.size)
+# print(self_img.filename)
+# print(self_img.format)
+# print(self_img.format_description)
 
 
 # Save the image
 # Conversion only required while saving RGBA file to jpg format
-self_img = self_img.convert("RGB")
-self_img.save("dp.jpg")
+# self_img = self_img.convert("RGB")
+# self_img.save("dp.jpg")
+
+
+# Creating new image
+image_1 = Image.new("RGBA", (100, 200), "purple")
+image_1.save("purple_image.png")
