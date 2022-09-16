af5cb3a5c7d2509a37d746f9794536b51471a996
IamSarma <sarmaakondivnm@gmail.com>
2022-09-16 21:49:42 +0530

    Manipulating image(s) - Getting RGBA values that user want to use

---
+++ python/104_manipulating_images.py
@@ -0,0 +1,8 @@
+from PIL import ImageColor
+
+
+# Getting RGBA values that user want to use
+print(ImageColor.getcolor("red", "RGBA"))
+print(ImageColor.getcolor("black", "RGBA"))
+print(ImageColor.getcolor("chocolate", "RGBA"))
+print(ImageColor.getcolor("aliceblue", "RGBA"))
