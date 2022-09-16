a9bfe3436b537c45f32e53ae8e4c1d1236885af1
IamSarma <sarmaakondivnm@gmail.com>
2022-09-16 21:08:40 +0530

    Get message body and send

--- python/103_sending_text_messages.py
+++ python/103_sending_text_messages.py
@@ -10,3 +10,8 @@ twilio_cli = Client(account_SID, auth_token)
 # Get twilio and user's number
 twilio_number = input("Enter twilio number: ")
 self_number = input("Enter your number: ")
+
+# Get message body and send
+message_body = input("Enter message body: ")
+user_message = twilio_cli.messages.create(
+    body=message_body, from_=twilio_number, to=self_number)
