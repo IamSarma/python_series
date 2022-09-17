873e34b83d744296a4182a0148810e6aa0b4a56e
IamSarma <sarmaakondivnm@gmail.com>
2022-09-17 10:01:54 +0530

    Save change(s)

--- python/105_resize_and_addlogo.py
+++ python/105_resize_and_addlogo.py
@@ -40,3 +40,6 @@ for file_name in os.listdir("."):
         print(f"Adding logo to {file_name}")
         target_img.paste(logo_image, (target_img_width -
                          logo_image_width, target_img_height - logo_image_height))
+
+        # Save changes
+        target_img.save(os.path.join(file_name, "with_logo"))
