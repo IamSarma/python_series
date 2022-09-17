379933d2296e37fd684be4bc31fbca5bb956fb65
IamSarma <sarmaakondivnm@gmail.com>
2022-09-17 12:34:45 +0530

    Make a thin black outline at the edges of the image and saving the result image

--- python/106_drawing_on_images.py
+++ python/106_drawing_on_images.py
@@ -6,4 +6,12 @@ new_image = Image.new("RGBA", (200, 200), "white")
 
 
 # Creating image draw object
-img_draw_obj = ImageDraw.Draw(new_image)
+draw_obj = ImageDraw.Draw(new_image)
+
+
+# Make a thin black outline at the edges of the image
+draw_obj.line([(0, 0), (199, 0), (199, 199), (0, 199), (0, 0)], fill="black")
+
+
+# Save the resulting image
+new_image.save("drawing_img.png")
