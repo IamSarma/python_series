3c4e0a2139f55870560a332b7093ccdb9dbd31ee
IamSarma <sarmaakondivnm@gmail.com>
2022-09-17 19:24:13 +0530

    Moving the mouse in square pattern from fixed start position

--- python/107_gui_automation.py
+++ python/107_gui_automation.py
@@ -2,6 +2,15 @@ import pyautogui
 
 
 # Obtaining the screen resolution
-screen_width, screen_height = pyautogui.size()
-print(f"The current display's width is: {screen_width} pixels")
-print(f"The current display's height is: {screen_height} pixels")
+# screen_width, screen_height = pyautogui.size()
+# print(pyautogui.size())
+# print(f"The current display's width is: {screen_width} pixels")
+# print(f"The current display's height is: {screen_height} pixels")
+
+
+# Moving the mouse in square pattern from fixed start position
+for i in range(5):
+    pyautogui.moveTo(100, 100, duration=0.25)
+    pyautogui.moveTo(200, 100, duration=0.25)
+    pyautogui.moveTo(200, 200, duration=0.25)
+    pyautogui.moveTo(100, 200, duration=0.25)
