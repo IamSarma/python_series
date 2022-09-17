e6c6f9d1daa6eccfd393929f51193a5899acc448
IamSarma <sarmaakondivnm@gmail.com>
2022-09-17 19:57:39 +0530

    Drawing spiral

--- python/108_spiral_draw.py
+++ python/108_spiral_draw.py
@@ -1,10 +1,19 @@
 import pyautogui
 import time
 
-
 # Pause for 5 seconds
 time.sleep(5)
 
-
 # Click to make the windows active
 pyautogui.click()
+
+# Drawing spiral
+distance_px = 600
+change_px = 40
+while distance_px > 0:
+    pyautogui.drag(distance_px, 0, duration=0.25)
+    distance_px -= change_px
+    pyautogui.drag(0, distance_px, duration=0.25)
+    pyautogui.drag(-distance_px, 0, duration=0.25)
+    distance_px -= change_px
+    pyautogui.drag(0, -distance_px, duration=0.25)
