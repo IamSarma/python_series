76a2d1d74ce24e109ec3cd7e2efc35830b8ea391
IamSarma <sarmaakondivnm@gmail.com>
2022-09-17 12:35:51 +0530

    Draw a blue rectangle

--- python/106_drawing_on_images.py
+++ python/106_drawing_on_images.py
@@ -13,5 +13,9 @@ draw_obj = ImageDraw.Draw(new_image)
 draw_obj.line([(0, 0), (199, 0), (199, 199), (0, 199), (0, 0)], fill="black")
 
 
+# Draw a blue rectangle
+draw_obj.rectangle((20, 30, 60, 60), fill="blue")
+
+
 # Save the resulting image
 new_image.save("drawing_img.png")
