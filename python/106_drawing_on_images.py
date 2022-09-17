9420c423a4a01302472647fc6b908cfc09c8c80a
IamSarma <sarmaakondivnm@gmail.com>
2022-09-17 12:26:35 +0530

    Creating image draw object

--- python/106_drawing_on_images.py
+++ python/106_drawing_on_images.py
@@ -3,3 +3,7 @@ from PIL import Image, ImageDraw
 
 # Creating image object
 new_image = Image.new("RGBA", (200, 200), "white")
+
+
+# Creating image draw object
+img_draw_obj = ImageDraw.Draw(new_image)
