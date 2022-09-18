3604be90012a89e30844e09bbfa517f64b72a979
IamSarma <sarmaakondivnm@gmail.com>
2022-09-18 12:38:47 +0530

    Write some text in to the notepad window

--- python/112_read_text_from_notepad.py
+++ python/112_read_text_from_notepad.py
@@ -12,3 +12,8 @@ time.sleep(2)
 # Create notepad window object and maximize the window
 notepad_window = pyautogui.getActiveWindow()
 notepad_window.maximize()
+
+# Write some text in to the notepad window
+time.sleep(1)
+pyautogui.write(
+    "This text is written in notepad using pyautogui, subprocess and time modules and copied using pyperclip")
