ee322194341a00cb856f0dcef321b6dc8d1b927d
IamSarma <sarmaakondivnm@gmail.com>
2022-09-18 12:36:35 +0530

    Create notepad window object and maximize the window

--- python/112_read_text_from_notepad.py
+++ python/112_read_text_from_notepad.py
@@ -2,7 +2,13 @@
 import pyautogui
 import pyperclip
 import subprocess
+import time
 
 
 # Open Notepad application
 subprocess.Popen("notepad.exe")
+time.sleep(2)
+
+# Create notepad window object and maximize the window
+notepad_window = pyautogui.getActiveWindow()
+notepad_window.maximize()
