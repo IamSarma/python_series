75010fd84a8ed143c24d912e926800073b697a34
IamSarma <sarmaakondivnm@gmail.com>
2022-09-18 10:30:26 +0530

    Program that fills in the form (google form) automatically, imported required module(s) and listed out TODOs

---
+++ python/109_form_filler.py
@@ -0,0 +1,25 @@
+#! python 2
+# form_filler.py - Automatically fills in the form
+import pyautogui
+import time
+
+
+# Give the user a chance to kill the script
+
+# Wait until the form page has loaded
+
+# Fill out the Name field
+
+# Fill out the Greatest Fear(s) field
+
+# Fill out the Source of Wizard Powers field
+
+# Fill out the Robocop field
+
+# Fill out the Additional Comments field
+
+# Click Sumbit
+
+# Wait until form page has loaded
+
+# Click the Submit another response link
