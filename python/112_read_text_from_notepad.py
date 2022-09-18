15aad16ec4586253202e1f702a0fd7ef46431083
IamSarma <sarmaakondivnm@gmail.com>
2022-09-18 12:41:42 +0530

    Copy all the content from notepad window

--- python/112_read_text_from_notepad.py
+++ python/112_read_text_from_notepad.py
@@ -7,7 +7,7 @@ import time
 
 # Open Notepad application
 subprocess.Popen("notepad.exe")
-time.sleep(2)
+time.sleep(1)
 
 # Create notepad window object and maximize the window
 notepad_window = pyautogui.getActiveWindow()
@@ -17,3 +17,9 @@ notepad_window.maximize()
 time.sleep(1)
 pyautogui.write(
     "This text is written in notepad using pyautogui, subprocess and time modules and copied using pyperclip")
+
+# Copy all the content from notepad window
+time.sleep(0.5)
+pyautogui.hotkey("ctrl", "a")
+time.sleep(0.5)
+pyautogui.hotkey("ctrl", "c")
