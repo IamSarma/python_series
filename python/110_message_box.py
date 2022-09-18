bc795d8fc4a00e664f7f80df31d266b603502253
IamSarma <sarmaakondivnm@gmail.com>
2022-09-18 11:58:19 +0530

    Displaying message boxe(s) - confirm

--- python/110_message_box.py
+++ python/110_message_box.py
@@ -4,3 +4,6 @@ import pyautogui
 # Displaying message boxe(s)
 # Alert
 pyautogui.alert("This is a critical message", "Important")
+
+# Confirm
+pyautogui.confirm("Do you want to continue?", "Attention Required")
