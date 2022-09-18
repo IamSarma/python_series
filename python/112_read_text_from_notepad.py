9c8b4eddb4bd9106b51f62e5a558cd975425a595
IamSarma <sarmaakondivnm@gmail.com>
2022-09-18 12:24:09 +0530

    Program to read text from Notepad (with help of clipboard)

---
+++ python/112_read_text_from_notepad.py
@@ -0,0 +1,8 @@
+# Program to read text from notepad (gets text from clipboard)
+import pyautogui
+import pyperclip
+import subprocess
+
+
+# Open Notepad application
+subprocess.Popen("notepad.exe")
