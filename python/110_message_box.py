dfb917e17dd4e1bf51223ab229d9db4ffadf4a8f
IamSarma <sarmaakondivnm@gmail.com>
2022-09-18 11:59:43 +0530

    Displaying message boxe(s) - prompt

--- python/110_message_box.py
+++ python/110_message_box.py
@@ -3,7 +3,11 @@ import pyautogui
 
 # Displaying message boxe(s)
 # Alert
-pyautogui.alert("This is a critical message", "Important")
+# pyautogui.alert("This is a critical message", "Important")
 
 # Confirm
-pyautogui.confirm("Do you want to continue?", "Attention Required")
+# pyautogui.confirm("Do you want to continue?", "Attention Required")
+
+# Prompt
+user_name = pyautogui.prompt("Enter username: ", "Login")
+print(user_name)
